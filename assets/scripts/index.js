8c18d458ce2f8c689e275a76fcd361444690f21c
Heribert <crossbrand96@gmail.com>
2023-08-20 21:56:24 +0100

    branch factorial commit

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,9 @@
-
-
-function factorial(x){
-  if(x === 0 || x === 1){
+function factorial(x) {
+  if (x === 0 || x === 1) {
     return 1;
   }
-  else{
-    return x * factorial(x-1);
+  else {
+    return x * factorial (x-1)
   }
 }
 
